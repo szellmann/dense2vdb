4f64cbd939c27691bf676b5ae02bc8374ce419c1
Stefan Zellmann <info@szellmann.de>
2025-02-02 19:11:35 +0100

    Add ZFP comparison

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include <openvdb/openvdb.h>
 #include <openvdb/tools/DenseSparseTools.h>
 #include <openvdb/math/Math.h>
+// zfp
+#ifdef WITH_ZFP
+#include <zfp.h>
+#endif
 // ours
 #include "math_.h"
 #include "nanovdb_convert.h"
@@ -26,6 +30,7 @@ static int3 g_dims{0,0,0};
 static std::string g_type = "float";
 static double g_compressionRate{0.5};
 static bool g_stats{false};
+static bool g_zfp{false};
 // outdated, we're keeping these for testing though:
 static float g_backgroundValue{NAN};
 static float g_tolerance{NAN};
@@ -44,13 +49,15 @@ static bool parseCommandLine(int argc, char **argv)
       g_type = argv[++i];
     else if (arg == "-c" || arg == "compressionRate")
       g_compressionRate = atof(argv[++i]);
+    else if (arg == "-stats")
+      g_stats = true;
+    else if (arg == "-zfp")
+      g_zfp = true;
     /* deprecated:*/
     else if (arg == "-background" || arg == "-bg")
       g_backgroundValue = atof(argv[++i]);
     else if (arg == "-tolerance")
       g_tolerance = atof(argv[++i]);
-    else if (arg == "-stats")
-      g_stats = true;
     /* END deprecated */
     else if (arg[0] != '-')
       g_inFileName = arg;
@@ -118,6 +125,19 @@ float getValue(const char *input, int x, int y, int z)
 }
 
 inline
+float getValue(const float *data, int x, int y, int z)
+{
+  return data[x+y*g_dims.x+z*size_t(g_dims.x)*g_dims.y];
+}
+
+inline
+float getValue(nanovdb::NanoGrid<float> *nvdb, int x, int y, int z)
+{
+  auto acc = nvdb->getAccessor();
+  return acc.getValue(nanovdb::Coord(x,y,z));
+}
+
+inline
 float mapValue(float value, float minValue, float maxValue)
 {
   return (value-minValue)/(maxValue-minValue);
@@ -131,11 +151,10 @@ struct Stats
   double snr{0.0};
 };
 
-static
-Stats computeStats(const char *input, nanovdb::NanoGrid<float> *nvdb)
+template <typename Compressed>
+Stats computeStats(const char *input, Compressed comp)
 {
   Stats res;
-  auto acc = nvdb->getAccessor();
 
   // compute min/max's
   for (int z=0; z<g_dims.z; ++z) {
@@ -145,7 +164,7 @@ Stats computeStats(const char *input, nanovdb::NanoGrid<float> *nvdb)
         res.minValue = fminf(res.minValue,value0);
         res.maxValue = fmaxf(res.maxValue,value0);
 
-        float value1 = acc.getValue(nanovdb::Coord(x,y,z));
+        float value1 = getValue(comp,x,y,z);
         res.minVDB = fminf(res.minVDB,value1);
         res.maxVDB = fmaxf(res.maxVDB,value1);
       }
@@ -158,8 +177,7 @@ Stats computeStats(const char *input, nanovdb::NanoGrid<float> *nvdb)
     for (int y=0; y<g_dims.y; ++y) {
       for (int x=0; x<g_dims.x; ++x) {
         float value0 = mapValue(getValue(input,x,y,z), res.minValue, res.maxValue);
-        float value1
-          = mapValue(acc.getValue(nanovdb::Coord(x,y,z)), res.minVDB, res.maxVDB);
+        float value1 = mapValue(getValue(comp,x,y,z), res.minVDB, res.maxVDB);
         double sqr = double(value0) * double(value0);
         sumSquared += sqr;
         double diff = double(value0) - double(value1);
@@ -175,6 +193,43 @@ Stats computeStats(const char *input, nanovdb::NanoGrid<float> *nvdb)
   else res.snr = 20*log10(sqrt(signalMean)/sqrt(noiseMean));
   return res;
 }
+
+#ifdef WITH_ZFP
+std::vector<uchar> zfpCompress(std::vector<float> &input)
+{
+  uint dims = 3;
+  zfp_type type = zfp_type_float;
+  zfp_field* field = zfp_field_3d(input.data(), type, g_dims.x, g_dims.y, g_dims.z); 
+  zfp_stream* zfp = zfp_stream_open(NULL);
+  zfp_stream_set_rate(zfp, g_compressionRate, type, dims, 0);
+  //zfp_stream_set_precision(zfp, precision, type);
+  //zfp_stream_set_accuracy(zfp, tolerance, type);
+  size_t bufsize = zfp_stream_maximum_size(zfp, field);
+  std::vector<uchar> buffer(bufsize);
+  bitstream* stream = stream_open(buffer.data(), bufsize);
+  zfp_stream_set_bit_stream(zfp, stream);
+  zfp_stream_rewind(zfp);
+  std::cout << "compress...\n";
+  size_t size = zfp_compress(zfp, field);
+  buffer.resize(size); // adjust to actual size
+  return buffer;
+}
+
+std::vector<float> zfpDecompress(std::vector<uchar> &buffer)
+{
+  uint dims = 3;
+  std::vector<float> result(g_dims.x*size_t(g_dims.y)*g_dims.z);
+  zfp_type type = zfp_type_float;
+  zfp_field* field = zfp_field_3d(result.data(), type, g_dims.x, g_dims.y, g_dims.z); 
+  zfp_stream* zfp = zfp_stream_open(NULL);
+  zfp_stream_set_rate(zfp, g_compressionRate, type, dims, 0);
+  bitstream* stream = stream_open(buffer.data(), buffer.size());
+  zfp_stream_set_bit_stream(zfp, stream);
+  zfp_stream_rewind(zfp);
+  size_t size = zfp_decompress(zfp, field);
+  return result;
+}
+#endif
 
 int main(int argc, char **argv)
 {
@@ -215,37 +270,65 @@ int main(int argc, char **argv)
   std::cout << "Dims: " << g_dims.x << ':' << g_dims.y << ':' << g_dims.z << '\n';
   std::cout << "Type: " << g_type << '\n';
 
-  d2nvdbParams parms;
-  parms.dims[0] = g_dims.x;
-  parms.dims[1] = g_dims.y;
-  parms.dims[2] = g_dims.z;
-  parms.type = g_type.c_str();
-  parms.compressionRate = g_compressionRate;
+  if (g_zfp) { // ZFP compression (only for comparison, output file is *not* compressed!):
+#ifdef WITH_ZFP
+    // convert to float:
+    std::vector<float> inputf;
+    for (int z=0; z<g_dims.z; ++z) {
+      for (int y=0; y<g_dims.y; ++y) {
+        for (int x=0; x<g_dims.x; ++x) {
+          inputf.push_back(getValue(input.data(),x,y,z));
+        }
+      }
+    }
+    auto buffer = zfpCompress(inputf);
+    std::cout << "Compression achieved: "
+      << buffer.size()/double(g_dims.x*size_t(g_dims.y)*g_dims.z) << '\n';
+    auto decomp = zfpDecompress(buffer);
+    if (1 || g_stats) { // stats, that's what this test is for...
+      auto s = computeStats(input.data(), decomp.data());
+      std::cout << "min/max (in) ....: [" << s.minValue << ',' << s.maxValue << "]\n";
+      std::cout << "min/max (out) ...: [" << s.minVDB << ',' << s.maxVDB << "]\n";
+      std::cout << "MSE .............: " << s.mse << '\n';
+      std::cout << "SNR .............: " << s.snr << '\n';
+    }
 
-  uint64_t bufferSize;
-  d2nvdbCompress(input.data(), &parms, nullptr, &bufferSize);
+    std::ofstream os(g_outFileName, std::ios::out | std::ios::binary);
+    os.write((const char *)decomp.data(), g_dims.x*size_t(g_dims.y)*g_dims.z * sizeof(float));
+#endif
+  } else { // VDB compression ("ours"):
+    d2nvdbParams parms;
+    parms.dims[0] = g_dims.x;
+    parms.dims[1] = g_dims.y;
+    parms.dims[2] = g_dims.z;
+    parms.type = g_type.c_str();
+    parms.compressionRate = g_compressionRate;
 
-  constexpr size_t alignment = 32;
-  char* alignedBuffer = (char*)std::aligned_alloc(alignment, bufferSize);
+    uint64_t bufferSize;
+    d2nvdbCompress(input.data(), &parms, nullptr, &bufferSize);
 
-  d2nvdbCompress(input.data(), &parms, alignedBuffer, &bufferSize);
+    constexpr size_t alignment = 32;
+    char* alignedBuffer = (char*)std::aligned_alloc(alignment, bufferSize);
 
-  auto nvdbBuffer = nanovdb::HostBuffer::createFull(bufferSize, alignedBuffer);
-  nanovdb::GridHandle<nanovdb::HostBuffer> handle = std::move(nvdbBuffer);
+    d2nvdbCompress(input.data(), &parms, alignedBuffer, &bufferSize);
 
-  if (g_stats) {
-    auto s = computeStats(input.data(), handle.grid<float>());
-    std::cout << "min/max (in) ....: [" << s.minValue << ',' << s.maxValue << "]\n";
-    std::cout << "min/max (out) ...: [" << s.minVDB << ',' << s.maxVDB << "]\n";
-    std::cout << "MSE .............: " << s.mse << '\n';
-    std::cout << "SNR .............: " << s.snr << '\n';
+    auto nvdbBuffer = nanovdb::HostBuffer::createFull(bufferSize, alignedBuffer);
+    nanovdb::GridHandle<nanovdb::HostBuffer> handle = std::move(nvdbBuffer);
+
+    if (g_stats) {
+      auto s = computeStats(input.data(), handle.grid<float>());
+      std::cout << "min/max (in) ....: [" << s.minValue << ',' << s.maxValue << "]\n";
+      std::cout << "min/max (out) ...: [" << s.minVDB << ',' << s.maxVDB << "]\n";
+      std::cout << "MSE .............: " << s.mse << '\n';
+      std::cout << "SNR .............: " << s.snr << '\n';
+    }
+
+    std::ofstream os(g_outFileName, std::ios::out | std::ios::binary);
+    nanovdb::io::Codec       codec = nanovdb::io::Codec::NONE;// compression codec for the file
+    nanovdb::io::writeGrid(os, handle, codec);
+
+    std::free(alignedBuffer);
   }
-
-  std::ofstream os(g_outFileName, std::ios::out | std::ios::binary);
-  nanovdb::io::Codec       codec = nanovdb::io::Codec::NONE;// compression codec for the file
-  nanovdb::io::writeGrid(os, handle, codec);
-
-  std::free(alignedBuffer);
 
   return 0;
 }
